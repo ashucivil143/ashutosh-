e6b6a0444b159332bbe158668562894abbd1b85c
ashucivil143 <shuklaashutosh143@gmail.com>
2019-12-16 18:50:21 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,j,sum;
+printf("enter the first number");
+scanf("%d",&i);
+printf("enter the second number");
+scanf("%d",&j);
+sum=i+j;
+printf("the sum is %d",sum);
+return 0;
+}                        
